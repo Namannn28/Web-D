62a13bf7c9606037f796bb9f7407ce73e2dbcafc
Naman Gupta <namang380@gmail.com>
2026-06-28 00:20:25 +0530

    lec 45 examples

---
+++ MERN Stack notes/video44/script.js
@@ -0,0 +1,6 @@
+// let items = ["Love", "Babbar", "True"];
+// items.splice(1, 1, "Kunal"); // At index 1, delete 1 item, then insert "Kunal" [00:21:43]
+// console.log(items); // ["Love", "Kunal", "True"]
+
+let arr = [1,2343,3,4,50,5]
+console.log(arr.sort().reverse())
